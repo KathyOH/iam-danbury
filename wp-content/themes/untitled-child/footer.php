815f4fcd2afc5cdf24929f0ab84773d5286ad924
Kathy Hoile <kohoile@outlook.com>
2017-01-10 17:52:34 -0500

    styled facebook logo and reorganized divs in footer

--- wp-content/themes/untitled-child/footer.php
+++ wp-content/themes/untitled-child/footer.php
@@ -14,20 +14,23 @@
 	<div id="colophon-wrap">
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="site-info">
-				<p class="iam-footer">
-					<span><b>IAM Interfaith AIDS Ministry of Greater Danbury</b></span><br>
+				<div class="iam-footer">
+					<p class="footer-info">
+						<span><b>IAM Interfaith AIDS Ministry of Greater Danbury</b></span><br>
 							54 Main St., Danbury, CT 06810<br>
 							Phone: (203) 748-4077<br>
 							Fax: (203) 748-2841<br>
 							Email: <a href="mailto:iamlucyc@sbcglobal.net">iamlucyc@sbcglobal.net</a>	
-				</p>	
+					</p>	
+			
 				
-				<div class="facebook-link">
-					<a href="https://www.facebook.com/iamofgreaterdanbury/" target="_blank"> 
-						<i class="fa fa-facebook fa-4x" href="" title="Facebook" target="_blank"></i>
-					</a>
-				</div>
+					<div class="facebook-link">
+						<a href="https://www.facebook.com/iamofgreaterdanbury/" target="_blank"> 
+							<i class="fa fa-facebook fa-4x" href="" title="Facebook" target="_blank"></i>
+						</a>
+					</div><!-- .facebook-link -->
 				
+				</div><!-- .iam-footer -->
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div><!-- #colophon-wrap -->
