d519b07217c7c54a3f93e0ac4f37438449d4c95e
Kathy Hoile <kohoile@outlook.com>
2017-01-10 09:58:26 -0500

    added Facebook link but no logo to footer

--- wp-content/themes/untitled-child/footer.php
+++ wp-content/themes/untitled-child/footer.php
@@ -14,15 +14,17 @@
 	<div id="colophon-wrap">
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="site-info">
-				
-			<p class="iam-footer"><span><b>IAM Interfaith AIDS Ministry of Greater Danbury</b></span><br>54 Main St., Danbury, CT 06810<br>Phone: (203) 748-4077<br>Fax: (203) 748-2841<br>Email: <a href="mailto:iamlucyc@sbcglobal.net">iamlucyc@sbcglobal.net</a>
-				
-			
-				
+				<p class="iam-footer">
+					<span><b>IAM Interfaith AIDS Ministry of Greater Danbury</b></span><br>
+							54 Main St., Danbury, CT 06810<br>
+							Phone: (203) 748-4077<br>
+							Fax: (203) 748-2841<br>
+							Email: <a href="mailto:iamlucyc@sbcglobal.net">iamlucyc@sbcglobal.net</a>	
 				</p>	
 				
-				
-				
+				<div class="facebook-link">
+					<a href="https://www.facebook.com/iamofgreaterdanbury/"><img src="img/facebook_logo.png"></a>
+				</div>
 				
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
